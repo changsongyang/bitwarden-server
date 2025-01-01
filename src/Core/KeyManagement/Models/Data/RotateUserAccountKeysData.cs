c2feea3350aed4da43be5786cf441fe62afc619d
Bernd Schoolmann <mail@quexten.com>
2025-01-01 18:04:22 +0100

    Dotnet format

--- src/Core/KeyManagement/Models/Data/RotateUserAccountKeysData.cs
+++ src/Core/KeyManagement/Models/Data/RotateUserAccountKeysData.cs
@@ -1,5 +1,5 @@
-﻿using Bit.Core.Auth.Models.Data;
-using Bit.Core.Auth.Entities;
+﻿using Bit.Core.Auth.Entities;
+using Bit.Core.Auth.Models.Data;
 using Bit.Core.Entities;
 using Bit.Core.Tools.Entities;
 using Bit.Core.Vault.Entities;
